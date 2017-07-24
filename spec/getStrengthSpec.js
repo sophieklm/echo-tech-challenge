aad0b75c9c840e95ab6cf7500a7084dd4ee97ff1
sophieklm <sklm@live.co.uk>
2017-07-24 11:26:54 +0100

    returns numerator and denominator values

--- spec/getStrengthSpec.js
+++ spec/getStrengthSpec.js
@@ -2,8 +2,48 @@ var extractor = require('../src/getStrength');
 
 describe("getStrength", function() {
 
-  it('gets the numerator value of strength', function() {
-    expect(extractor.getStrength('Oxcarbazepine 600mg tablets').numerator_value).toEqual(600);
+  describe("numerator", function() {
+
+    it('gets the numerator value of strength', function() {
+      expect(extractor.getStrength('Oxcarbazepine 600mg tablets').numerator_value).toEqual(600);
+    });
+
+    it('gets the numerator value of strength for a float', function() {
+      expect(extractor.getStrength('Glucosamine sulfate 1.5g tablets').numerator_value).toEqual(1.5);
+    });
+
+    it('returns null if no value present', function() {
+      expect(extractor.getStrength('Aveeno Intense Relief hand cream').numerator_value).toEqual(null);
+    });
+
+    // it('returns the units for mg', function() {
+    //   expect(extractor.getStrength('Sumatriptan 100mg tablets').numerator_unit).toEqual('mg');
+    // });
+    //
+    // it('returns units for %', function() {
+    //   expect(extractor.getStrength('Betnovate 0.1% cream').numerator_unit).toEqual('%');
+    // });
+
+    it('returns null if no units', function() {
+      expect(extractor.getStrength('NatraVits Vitamin B Complex tablets').numerator_unit).toEqual(null);
+    });
+
+    it('only returns first digit if denominator present', function() {
+      expect(extractor.getStrength('Indometacin 5mg/5ml oral solution').numerator_value).toEqual(5);
+    });
+
+  });
+
+  describe("denominator", function() {
+
+    it('returns the denominator value', function() {
+      expect(extractor.getStrength('Lisinopril 14mg/5ml oral solution').denominator_value).toEqual(5);
+    });
+
+    // it('returns the units', function() {
+    //   expect(extractor.getStrength('Indometacin 5mg/5ml oral solution').denominator_unit).toEqual('ml');
+    // });
+
   });
 
 });
--- src/getStrength.js
+++ src/getStrength.js
@@ -8,11 +8,36 @@ exports.getStrength = function(string) {
 function strength(string) {
     this.numerator_value = set_numerator_value(string);
     this.numerator_unit = null;
-    this.denominator_value = null;
+    this.denominator_value = set_denominator_value(string);
     this.demoninator_unit = null;
 }
 
 function set_numerator_value(string) {
-  var string_num = string.replace( /(^.+\D)(\d+)(\D.+$)/i,'$2');
-  return parseFloat(string_num);
+  if (extract_numbers(string)) {
+    return extract_numbers(string)[0];
+  }
+  else {
+    return null;
+  }
+}
+
+function set_denominator_value(string) {
+  if (extract_numbers(string)) {
+    return extract_numbers(string)[extract_numbers(string).length-1];
+  }
+  else {
+    return null;
+  }
+}
+
+function extract_numbers(string) {
+  var regex = /[+-]?\d+(\.\d+)?/g;
+  var floats = string.match(regex);
+  if (floats) {
+    return floats.map(function(v) { return parseFloat(v); });
+  }
+}
+
+function set_numerator_unis(string) {
+  return string.replace( /(^.+\D)(\d+)(\D.+$)/i,'$2');
 }
